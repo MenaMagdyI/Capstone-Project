0987faeb4524f52e7e134fc0e5f27d1c63037bcf
MenaMagdyI <menamagdyibrahim@outlook.com>
2018-03-14 12:18:27 +0200

    first commit

---
+++ app/src/main/java/trendy/mina/com/trendy/MainActivity.java
@@ -0,0 +1,13 @@
+package trendy.mina.com.trendy;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
