1abd239680f378dbeb252a6c412d93fcb10a73ee
MenaMagdyI <menamagdyibrahim@outlook.com>
2018-03-24 02:39:30 +0200

    fix Content provider bug

--- app/src/main/java/trendy/mina/com/trendy/MainActivity.java
+++ app/src/main/java/trendy/mina/com/trendy/MainActivity.java
@@ -10,6 +10,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.util.Log;
 import android.view.View;
 import android.widget.ProgressBar;
 import android.widget.TextView;
@@ -108,6 +109,7 @@ public class MainActivity extends AppCompatActivity implements LoaderManager.Loa
                     @Override
                     protected Void doInBackground(Void... voids) {
                         ContentProviderUtils.insertSourcesIntoContentProvider(MainActivity.this, data);
+                        Log.i("after finish loader","111111111111");
                         return null;
                     }
                 }.execute();
